fadd250ffc86698dba0d2d025fbf7b246e7dd91e
divannisaa <divannisae@gmail.com>
2023-10-02 17:37:23 +0700

    styling dashboard

--- src/views/DashboardPage/Dashboard.jsx
+++ src/views/DashboardPage/Dashboard.jsx
@@ -1,9 +1,34 @@
 import "./dashboard.css"
-import Content from "../../components/Content/Content"
+import Navbar from "../../components/Content/Navbar/Navbar.jsx"
 
 const Dashboard = () => {
     return <div className="dashboard__container">
-        <Content navbarText="Master Data / Announcement" informationText="Announcement"/>
+        <Navbar navbarText="Overview" />
+
+       
+        <div className="grafik__data__kehadiran">
+            {/* yg atas */}
+            <h1>Grafik Data Kehadiran / Tahun / 2023</h1>
+            <div className="keterangan">
+                <div className="box"><h1>Masuk</h1><h1>1</h1></div>
+                <div className="box"><h1>Terlambat</h1><h1>2</h1></div>
+                <div className="box"><h1>Absen</h1><h1>3</h1></div>
+                <div className="box"><h1>Cuti</h1><h1>4</h1></div>
+                <div className="box"><h1>Sakit</h1><h1>5</h1></div>
+
+            </div>
+                
+        </div>
+        
+        <div className="grafik__data__member">
+            <h1>Grafik Data Member / Hari</h1>
+        </div>
+
+        <div className="data__member">
+            <h1>Data Member</h1>
+        </div>
+
+        
     </div> 
 }
 
