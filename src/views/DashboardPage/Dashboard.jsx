fab753f5c867203138715eb9768c52ce16b743c0
divannisaa <divannisae@gmail.com>
2023-10-03 17:26:20 +0700

    menyelesaikan css dashboard

--- src/views/DashboardPage/Dashboard.jsx
+++ src/views/DashboardPage/Dashboard.jsx
@@ -21,7 +21,31 @@ const Dashboard = () => {
         </div>
         
         <div className="grafik__data__member">
+        
+
+            <div className="left__container__dashboard">
             <h1>Grafik Data Member / Hari</h1>
+                <div className="hari">
+                    <h2>Hari ini</h2>
+                        <div className="right">
+                            <div><h2>Today</h2></div>
+                            <div><h2>Yesterday</h2></div>
+                            </div>    
+                </div>
+                    <div>
+                        <h1>Grafik</h1>
+                    </div>
+            </div>
+
+            <div className="right__container__dashboard">
+                <div className="box1"><h1>Masuk</h1><h1>1</h1></div>
+                <div className="box1"><h1>Terlambat</h1><h1>2</h1></div>
+                <div className="box1"><h1>Absen</h1><h1>3</h1></div>
+                <div className="box1"><h1>Cuti</h1><h1>4</h1></div>
+                <div className="box1"><h1>Sakit</h1><h1>5</h1></div>
+                
+            </div>
+
         </div>
 
         <div className="data__member">
