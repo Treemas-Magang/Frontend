d752965dfb12a2180eae95e6944a807bc183070b
rizki febriansyah <rfebriansyah385@gmail.com>
2023-10-13 15:12:25 +0700

    membuat dropdownlist untuk sort di members

---
+++ src/components/atoms/DropdownList.jsx
@@ -0,0 +1,63 @@
+/* eslint-disable prettier/prettier */
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
+import { Color } from '../../utils/color';
+import { text } from '../../utils/text';
+
+const DropdownList = () => {
+    const [isSort, setIsSort] = useState('');
+
+    const hendleOption = (value) => {
+        setIsSort(value);
+    };
+    console.log(isSort);
+  return (
+    <View style={[styles.dropdown]}>
+      <TouchableOpacity onPress={() => hendleOption('nama')} style={styles.option}>
+        <Text style={styles.textOption}>Nama</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => hendleOption('jam_kehadiran')} style={styles.option}>
+        <Text style={styles.textOption}>Jam Kehadiran</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => hendleOption('project')} style={styles.option}>
+        <Text style={styles.textOption}>Project</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => hendleOption('work_from_home')} style={styles.option}>
+        <Text style={styles.textOption}>Work From Home</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => hendleOption('cuti')} style={styles.option}>
+        <Text style={styles.textOption}>Cuti / Sakit</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => hendleOption('tidak_masuk')} style={styles.option}>
+        <Text style={styles.textOption}>Tidak masuk</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default DropdownList;
+
+const styles = StyleSheet.create({
+    dropdown:{
+        width: 200,
+        backgroundColor: Color.green,
+        position: 'absolute',
+        zIndex: 10,
+        top: 60,
+        left: 30,
+        borderRadius: 10,
+        borderTopLeftRadius: 0,
+    },
+    option:{
+        width: '100%',
+        paddingVertical:10,
+        paddingHorizontal: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: Color.white,
+    },
+    textOption:{
+        fontFamily:text.regular,
+        fontSize: 12,
+        color: Color.white,
+    },
+})
